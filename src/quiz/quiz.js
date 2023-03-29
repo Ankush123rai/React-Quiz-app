067ccb27f1b908ea7788ce7c5ad40e5519a50b8d
Ankush123rai <ankush98rai@gamil.com>
2023-03-29 14:05:15 +0530

    react quiz-app

---
+++ src/quiz/quiz.js
@@ -0,0 +1,35 @@
+import React, { useRef } from "react";
+
+function Quiz({ Question, Options, cAnswer, Flag, handleSubmit }) {
+  let result = useRef(0);
+
+  function handleRadioBtn(e) {
+    let tempValue = e.target.value;
+    if (Number(cAnswer) == tempValue) {
+      result.current = result.current + 1;
+    }
+    handleSubmit(result.current);
+  }
+  return (
+    <div>
+      <h1>{Question}</h1>
+      {Options.map((element, index) => {
+        return (
+          <div key={index}>
+            <input
+              type="radio"
+              value={index + 1}
+              onChange={handleRadioBtn}
+              name="question"
+            />
+            <label>{element.option}</label>
+          </div>
+        );
+      })}
+
+      <p>{cAnswer}</p>
+    </div>
+  );
+}
+
+export default Quiz;
